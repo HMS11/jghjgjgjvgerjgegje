e05537c9c8517b34b1557ec5846fbb23ade3d4d3
HMS11 <34721123+HMS11@users.noreply.github.com>
2018-08-09 21:55:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -205,4 +205,86 @@ const Sra7a = [
  }
 });
 
+client.on('message', message => {
+    if (message.author.bot) return;
+     if (message.content === prefix + "help-en") {
+		 message.channel.send('**The Message Was Sent On Private**');
+            
+	
+		 
+
+
+ message.author.sendMessage(`
+ **
+
+╔[❖════════════❖]╗
+             Prefix = ' # '
+╚[❖════════════❖]╝
+
+╔[❖════════════❖]╗
+             Admin Commands
+╚[❖════════════❖]╝
+
+ ❖ *kick <mention > ➾ kick member from server
+
+ ❖ *clear ➾ clear chat ( not ready yet fixing)
+ 
+ ❖ *mute < mention > ➾ mute member
+
+ ❖ *unmute <mention> ➾ unmute member
+
+ ❖ *ct <name> ➾ create channel
+
+ ❖ *cv <name> create voice channel
+  
+ ❖ *bc <message> ➾ message all members in server
+
+
+╔[❖════════════❖]╗
+            General  Commands
+╚[❖════════════❖]╝
+
+❖ #roll <number> ➾ role 
+
+❖ #date ➾ see date
+
+❖ *member ➾ members info
+
+❖ #ser-av ➾ server avatar
+
+❖ #uptime ➾ to see uptime
+
+❖ #invs ➾ server invite link
+
+❖ #own ➾ bot owner
+
+❖ #id ➾ your id
+
+❖ #avatar ➾ your avatar account
+
+❖ #help-en ➾ help by arabic
+
+❖ #help-en ➾ help by english
+
+❖ #ping ➾ to see ping
+
+❖ #bot ➾ bot informations 
+
+❖ #server ➾ server informations 
+
+❖ #invite ➾ bot invite link
+
+╔[❖════════════❖]╗
+                    Welcome
+╚[❖════════════❖]╝
+
+**
+
+
+
+`);
+
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
