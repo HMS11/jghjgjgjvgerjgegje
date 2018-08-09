85a115f11cfbbedabd3408c962ef39cdd836f6b6
HMS11 <34721123+HMS11@users.noreply.github.com>
2018-08-09 17:08:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,64 +29,6 @@ client.user.setGame(`MOON`,"http://twitch.tv/S-F")
 });
 
 
-client.on('message' , message => {
-var prefix = "-"
-
-if (message.author.bot) return;
-if (message.content.startsWith(prefix + "contact")) {
-if (!message.channel.guild) return;
-
-
-
-let args = message.content.split(" ").slice(1).join(" ");
-
-
-
-client.users.get("312992639395954689").send(
-    "\n" + "**" + "● السيرفر :" + "**" +
-    "\n" + "**" + "» " + message.guild.name + "**" +
-    "\n" + "**" + " ● المرسل : " + "**" +
-    "\n" + "**" + "» " + message.author.tag + "**" +
-    "\n" + "**" + " ● الرسالة : " + "**" +
-    "\n" + "**" + args + "**")
-
-let embed = new Discord.RichEmbed()
-     .setAuthor(message.author.username, message.author.avatarURL)
-     .setDescription('📬 تم ارسال الرسالة الى صاحب البوت بنجاح')
-     .setThumbnail(message.author.avatarURL)
-     .setFooter("By : Elmusaui_GK and SpeedMC")
-                                                
-
-message.channel.send(embed);
-
-
-}
-    
-});
-
-client.on('message', message => {
-     var prefix ="+"; // هنا تقدر تغيير برفكس البوت
- if(message.content.startsWith(prefix +"server")){
-if(!message.channel.guild) return message.reply(' ');
-const millis = new Date().getTime() - message.guild.createdAt.getTime();
-const now = new Date();
-dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
-const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
-const days = millis / 1000 / 60 / 60 / 24;
-let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
-var embed  = new Discord.RichEmbed()
-.setAuthor(message.guild.name, message.guild.iconURL)
-.addField("🆔 Server ID:", message.guild.id,true)
-.addField("📅 Created On", message.guild.createdAt.toLocaleString(),true)
-.addField("👥 Members ",[${message.guild.memberCount}],true)
-.addField("🌍 Others " , message.guild.region,true)
-.addField(" 🔐 Roles ",**[${message.guild.roles.size}]** Role,true)
-.setColor('#000000')
-message.channel.sendEmbed(embed)
-
-}
-});
-
 
 
 client.login(process.env.BOT_TOKEN);
