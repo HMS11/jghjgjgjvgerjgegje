e38757255bd3e48a6eaffacedac8c20535b23ea6
HMS11 <34721123+HMS11@users.noreply.github.com>
2018-08-12 14:44:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,6 +29,27 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
 });
 
 
+const devs = ['' , 'ايدي اونر البوث الثاني' , '312992639395954689' , ''];
+client.on('message', message => {
+var prefix = "#";
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+
+if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`**${argresult}** : تم بنجاح تغيير الاسم ?`)
+  return message.reply("**تم تغيير الاسم البوت بنجاح ?**");
+} else
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.sendMessage(`**${argresult}** : تم تغيير صورة البوت بنجاح ?`);
+
+}
+});
+
+
 
 
 client.login(process.env.BOT_TOKEN);
