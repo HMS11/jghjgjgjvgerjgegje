2b1e8307d37265f7b8a136c77337e75eebc17bd9
HMS11 <34721123+HMS11@users.noreply.github.com>
2018-08-13 11:14:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -377,4 +377,28 @@ client.on('message', message => {
     }
 });
 
+const jackeo = , ''ايدي الاونر لثاني' , '312992639395954689']; //Jackeo  حقوقي
+client.on('message', message => { 
+var prefix = "+"; 
+  if (message.author.bot) return; 
+  if (!message.content.startsWith(prefix)) return; 
+    var argresult = message.content.split(` `).slice(1).join(' '); 
+      if (!jackeo.includes(message.author.id)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length); 
+  let args = message.content.split(" ").slice(1);  
+  if (command === "say")  { 
+  if(!message.channel.guild) return message.reply('** __This command only for servers⛔__  **');
+          message.delete() 
+    message.channel.sendMessage(args.join(" ")).catch(console.error); 
+if (command == "emb")    { 
+  if(!message.channel.guild) return message.reply('** __This command only for servers⛔__  **'); 
+    let say = new Discord.RichEmbed() 
+    .setDescription(args.join("  ")) 
+    .setColor("RANDOM") 
+    message.channel.sendEmbed(say); 
+    message.delete(); 
+  } 
+});
+
 client.login(process.env.BOT_TOKEN);
