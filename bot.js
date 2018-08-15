09edbd944e5908c64d85b4cd62d645a8b839f5b4
HMS11 <34721123+HMS11@users.noreply.github.com>
2018-08-15 13:17:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -445,37 +445,6 @@ client.on('message', message => {
 			dontSendBC.on('collect', r => {
 				msg.delete();
 				message.reply(':white_check_mark: **تم الغاء ارسال رسالتك بنجاح**').then(msg => msg.delete(5000));
-			});
-		})
-	}
-});
-
-client.on('message',message =>{
-    var prefix = "+";
-    if(message.content.startsWith(prefix + 'top')) {
-  message.guild.fetchInvites().then(i =>{
-  var invites = [];
-   
-  i.forEach(inv =>{
-    var [invs,i]=[{},null];
-     
-    if(inv.maxUses){
-        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
-    }else{
-        invs[inv.code] =+ inv.uses;
-    }
-        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
-   
-  });
-  var embed = new Discord.RichEmbed()
-  .setColor("#000000")
-  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
-  .setThumbnail("")
-           message.channel.send({ embed: embed });
-   
-  });
-   
-  
-
+                        });
 
 client.login(process.env.BOT_TOKEN);
